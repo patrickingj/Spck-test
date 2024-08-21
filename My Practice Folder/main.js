a1e130363e131db6988de57b68ef7a6e84275517
RikyEze <patrickingjr@gmail.com>
2024-08-21 16:13:51 +0100

    A test program

--- My Practice Folder/main.js
+++ My Practice Folder/main.js
@@ -1,58 +1,48 @@
-class Book {
-  
-  static totalBooks = 0;
-  
-  constructor(title, author, year) {
-    this.title = title,
-    this.author = author,
-    this.year = year
-    Book.totalBooks++;
+class Inventory {
+  constructor(name, price, quantity) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
   }
   
-  static getTotalBooks () {
-    return `There are ${Book.totalBooks} total books currently in this library`;
+  // Getter to calculate total value of the inventory
+  get totalValue() {
+    return this.price * this.quantity;
   }
   
+  // Setter to update the price with validation
+  set updatePrice(newPrice) {
+    if (newPrice < 0) {
+      console.error('Price cannot be a negative number');
+    } else {
+      this.price = newPrice;
+      console.log(`Price updated to ${newPrice}`);
+    }
+  }
+  
+  // Method to return product details
+  getProductDetails() {
+    return {
+      name: this.name,
+      price: this.price,
+      quantity: this.quantity
+    };
+  }
 }
 
-class EBook extends Book {
-  constructor(title, author, year, fileSize) {
-    
-    super (title, author, year);
-    this.fileSize = fileSize;
-  }
-  
-  getFileSizeWithUnit() {
-    return `${this.title} is ${this.fileSize} MB`;
-  }
-  
-}
+// Create instances of Inventory
+const product1 = new Inventory('Milo', 170, 10);
+const product2 = new Inventory('Peak', 200, 15);
+const product3 = new Inventory('Cowbell', 120, 5);
 
-class PrintedBook extends Book {
-  constructor(title, author, year, pageCount) {
-    
-    super (title, author, year);
-    this.pageCount = pageCount;
-  }
-  
-  getPageCountWithUnit() {
-    return `${this.title} has ${this.pageCount} pages`;
-  }
-  
-}
+// Use setter to update price
+product1.updatePrice = 200;
 
-const eBook1 = new EBook ('Eloquent JavaScript', 'Marijn Haverbeke', 2018, 4);
-const eBook2 = new EBook ('You Don’t Know JS: Scope & Closures', 'Kyle Simpson', 2014, 2);
-const eBook3 = new EBook ('JavaScript: The Definitive Guide', 'David Flanagan', 2020, 6);
+// Log total inventory value for product1
+console.log(`Total Value of ${product1.name}: ${product1.totalValue}`);
 
-const printedBook1 = new PrintedBook ('JavaScript: The Good Parts', 'Douglas Crockford', 2008, 176);
-const printedBook2 = new PrintedBook ('JavaScript Patterns', 'Stoyan Stefanov', 2010, 240);
-const printedBook3 = new PrintedBook ('Learning JavaScript Design Patterns', 'Addy Osmani', 2012, 254);
+// Destructure product details from product1
+const { name, price, quantity } = product1.getProductDetails();
+console.log(`Product Details -> Name: ${name}, Price: ${price}, Quantity: ${quantity}`);
 
-console.log(Book.getTotalBooks());
-
-console.log(eBook1);
-console.log(eBook1.getFileSizeWithUnit());
-
-console.log(printedBook1);
-console.log(printedBook1.getPageCountWithUnit());
+console.log(product1.getProductDetails());
