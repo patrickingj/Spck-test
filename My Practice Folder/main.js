a4a59fe2e9059ac0be4f88275ded99515ec29d0b
RikyEze <patrickingjr@gmail.com>
2024-08-22 16:37:06 +0100

    Did my final challenge on destructuring. I now have a solid knowledge of it

--- My Practice Folder/main.js
+++ My Practice Folder/main.js
@@ -29,3 +29,34 @@ const employee = {
 // My solution
 
 const {firstName, lastName, age: employeeAge, department: employeeDepartment, address : {city, zip}, responsibilities: [firstResponsibility]} = employee;
+
+// Final challenge on destructuring
+
+const library = {
+  librarian: { name: "Sarah", experience: 10 },
+  booksCount: 3,
+  books: [
+    {
+      title: "Eloquent JavaScript",
+      author: "Marijn Haverbeke",
+      year: 2018,
+      genres: ["Programming", "JavaScript", "Web Development"]
+    },
+    {
+      title: "You Don't Know JS",
+      author: "Kyle Simpson",
+      year: 2015,
+      genres: ["Programming", "JavaScript", "Education"]
+    },
+    {
+      title: "Clean Code",
+      author: "Robert C. Martin",
+      year: 2008,
+      genres: ["Programming", "Software Engineering", "Best Practices"]
+    }
+  ]
+};
+
+// My solution
+
+const {librarian: {name}, booksCount, books: [{title, author, year}, {title: secondBookTitle}, {genres: [firstGenre, , thirdGenre]}]} = library;
