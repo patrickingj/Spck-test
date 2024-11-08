8ca576c88b689fcb73ed7682ae87f618db83284f
RikyEze <patrickingjr@gmail.com>
2024-11-08 15:47:32 +0100

    update

--- My Practice Folder/main.js
+++ My Practice Folder/main.js
@@ -1,17 +1,30 @@
-const myDisplay = document.getElementById('calculator-screen');
+const choices = ['rock', 'paper', 'scissors'];
+const playersChoices = document.getElementById('playersChoices');
+const playerDisplay = document.getElementById('player');
+const computerDisplay = document.getElementById('computer');
+const gameResult = document.getElementById('message');
 
-function appendToDisplay(input) {
-  myDisplay.value += input;
-}
+let result;
 
-function clearDisplay() {
-  myDisplay.value = '';
-}
-
-function calculate() {
-  try {
-    myDisplay.value = eval(myDisplay.value);
-  } catch (e) {
-    myDisplay.value = 'Error';
+function playGame(playerChoice) {
+  let computerChoice = choices[Math.floor(Math.random() * 3)];
+  
+  if (playerChoice === computerChoice) {
+    console.log("It's a draw. TRY AGAIN");
+  } else {
+    switch (playerChoice) {
+      case 'rock':
+        result = (computerChoice === 'scissors') ? 'You WIN!' : 'You LOSE!'
+        break;
+      case 'paper':
+        result = (computerChoice === 'rock') ? 'You WIN!' : 'You LOSE!'
+        break;
+      case 'scissors':
+        result = (computerChoice === 'paper') ? 'You WIN!' : 'You LOSE!'
+        break;
+    }
+    playerDisplay.textContent = `Player's choice: ${playerChoice}`;
+    computerDisplay.textContent = `Computer's choice: ${computerChoice}`;
+    gameResult.textContent = result;
   }
 }
